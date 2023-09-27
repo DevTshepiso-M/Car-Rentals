ad5670a0c76461097fefd76fe545e3f440de7bb3
DevTshepiso-M <134273712+DevTshepiso-M@users.noreply.github.com>
2023-09-27 12:38:34 +0200

    Added "Payment" model

---
+++ Car Dealer/Models/Payment.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+namespace Car_Dealer.Models
+{
+	[DataContract]
+	public class Payment
+	{
+		[DataMember]
+		public Guid PaymentId { get; set; }
+		[DataMember]
+		public Guid RentalId { get; set; }
+		[DataMember]
+		public DateTime PaymentDate { get; set; }
+		[DataMember]
+		public decimal? PaymentAmount { get; set; }
+		[DataMember]
+		public string? PaymentMethod { get; set; }
+		[DataMember]
+        public string? PayStatus { get; set; }
+    }
+}
