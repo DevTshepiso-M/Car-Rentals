1d0128bd88d72eafb179e08bab83c7bc8e3c39c8
DevTshepiso-M <134273712+DevTshepiso-M@users.noreply.github.com>
2023-09-27 12:39:18 +0200

    Added User model

---
+++ Car Dealer/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace Car_Dealer.Models
+{
+	[DataContract]
+	public class User
+	{
+		[DataMember]
+		public Guid UserId { get; set; }
+		[DataMember]
+        public string? FirstName { get; set; }
+		[DataMember]
+        public string? LastName { get; set; }
+		[DataMember]
+		public string? Email { get; set; }
+		[DataMember]
+		public string? Password { get; set; }
+		[DataMember]
+		public string? Role { get; set; }
+		[DataMember]
+		public int? PhoneNumber { get; set;}
+    }
+}
