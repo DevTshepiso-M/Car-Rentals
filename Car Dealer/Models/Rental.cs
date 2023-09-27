8eddee7c22eaa03499464b525e93dc6c1a4e891b
DevTshepiso-M <134273712+DevTshepiso-M@users.noreply.github.com>
2023-09-27 12:39:03 +0200

    Added Rental model

---
+++ Car Dealer/Models/Rental.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace Car_Dealer.Models
+{
+	[DataContract]
+	public class Rental
+	{
+        [DataMember]
+        public Guid RentalID { get; set; }
+		[DataMember]
+		public Guid UserID { get; set;}
+		[DataMember]
+		public Guid CarID { get; set;}
+		[DataMember]
+		public DateOnly? RentalStartDate { get; set;}
+		[DataMember]
+		public DateOnly? RentalEndDate { get; set; }
+		[DataMember]
+		public decimal? TotalCost { get; set;}
+		[DataMember]
+		public string? Status { get; set;}
+	}
+}
