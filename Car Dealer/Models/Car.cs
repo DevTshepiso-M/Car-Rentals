e24015989d8b14f57966ab1dc08be883df52ae2f
DevTshepiso-M <134273712+DevTshepiso-M@users.noreply.github.com>
2023-09-27 12:38:01 +0200

    Added car model

---
+++ Car Dealer/Models/Car.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace Car_Dealer.Models
+{
+	[DataContract]
+	public class Car
+	{
+		[DataMember]
+		public Guid CarID { get; set; }
+		[DataMember]
+		public string? Make { get; set; }
+		[DataMember]
+		public string? Model { get; set; }
+		[DataMember]
+		public int? Year {  get; set; }
+		[DataMember]
+		public decimal? RegistrationNumber { get; set; }
+		[DataMember]
+		public string? Color { get; set; }
+		[DataMember]
+		public decimal? Mileage { get; set; }
+		[DataMember]
+		public decimal? DailyRentalRate { get; set; }
+		[DataMember]
+		public string? Description { get; set; }
+		[DataMember]
+		public string? AvailabilityStatus { get; set; }
+	}
+}
